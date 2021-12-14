5e58eb96ccf176fbf8c2538bc1448549a063ed32
Krishna Dhingra <dkrishna1608@gmail.com>
2021-12-14 09:20:33 +0530

    Removing the theme-dark class from all the components

--- src/components/chats/other-user-chat.js
+++ src/components/chats/other-user-chat.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function OtherUserChat() {
     return (
-        <div className="theme-dark mr-auto flex flex-col gap-1 w-max min-w-[200px] max-w-[450px]">
+        <div className="mr-auto flex flex-col gap-1 w-max min-w-[200px] max-w-[450px]">
 
             <span className="bg-chatSecondary w-min-content text-sm text-chatSecondary px-4 p-3 rounded-md rounded-bl-none text-left">Hello, my name is krishna dhingra Hello, my krishna dhingra Hello, my  krishna dhingra Hello, my </span>
             
