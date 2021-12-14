659e304a7676b86270275030feeb29a04b48bbe3
Krishna Dhingra <dkrishna1608@gmail.com>
2021-12-14 10:24:42 +0530

    Solving the mode swithcer button issue

--- src/components/chats/searchbar.js
+++ src/components/chats/searchbar.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function SearchBar() {
     return (
-        <input type="text" placeholder="Search for a message..." className="bg-inputBg px-3 py-2 rounded-md border outline-none border border-transparent focus:border-focus placeholder:text-sm max-w-[500px] w-full"></input>
+        <input type="text" placeholder="Search for a message..." className="bg-inputBg px-3 py-2 rounded-md text-primary outline-none border border-transparent focus:border-focus placeholder:text-sm max-w-[500px] w-full"></input>
     )
 }
