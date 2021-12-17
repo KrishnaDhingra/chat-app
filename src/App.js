a1a77a388bf087e1953bce4801d3f52580534c24
Krishna Dhingra <dkrishna1608@gmail.com>
2021-12-17 11:34:25 +0530

    Installing react-router and framer-motion and uninstalling unwanted files

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import OtherUsersContainer from './components/other-users/other-users-container.
 import MainChatsContainer from './components/chats/main-chats-container';
 
 function App() {
+
   const [ theme, setTheme ] = useState('theme-dark')
   const toggleTheme = () => {
     if(theme == 'theme-dark'){return setTheme('theme-light')}
     setTheme('theme-dark')
     console.log(theme)
   }
+  
   return (
     <div className={`${theme} main-container lg:max-h-[92vh] max-w-[1340px] w-screen h-screen bg-secondary grow self-stretch rounded-md shadow-xl`}>
       <Navbar/>
