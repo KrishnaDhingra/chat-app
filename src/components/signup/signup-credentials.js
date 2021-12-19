0284d3af13cef6251e9a55f3d4f388499e106834
Krishna Dhingra <dkrishna1608@gmail.com>
2021-12-20 01:20:35 +0530

    Setting linear backgound of the button and the borders of the buttons

--- src/components/signup/signup-credentials.js
+++ src/components/signup/signup-credentials.js
@@ -12,20 +12,20 @@ export default function SignupCredentials() {
             <input 
             placeholder="Teacher / Student Id" 
             type="text" 
-            className="rounded text-primary border-2 px-4 py-4 border-loginInput placeholder:text-primary outline-none bg-logininputBg focus:border-focus"
+            className="rounded text-primary border-[1.4px] px-4 py-4 border-loginInput placeholder:text-primary outline-none bg-logininputBg focus:border-focus"
             onChange={(e) => setId(e.target.value)} 
             value={id}/>
 
             <input 
             placeholder="8 Digit Pin" 
             type="password" 
-            className="rounded text-primary border-2 px-4 py-4 border-loginInput outline-none bg-logininputBg placeholder:text-primary focus:border-focus"
+            className="rounded text-primary border-[1.4px] px-4 py-4 border-loginInput outline-none bg-logininputBg placeholder:text-primary focus:border-focus"
             onChange={(e) => setPin(e.target.value)} 
             value={pin}/>
 
             <button 
             type='submit'
-            className="flex justify-between items-center bg-[#00E99F] rounded px-4 py-4 border-2 border-transparent font-semibold text-left"
+            className="text-primary signup-submit-button flex justify-between items-center bg-[#00E99F] rounded px-4 py-4 border-[1.4px] border-transparent font-semibold text-left"
             >Login to you Account <BsArrowRight className="text-xl"/></button>
         </form>   
     )
--- src/components/signup/signup-platforms.js
+++ src/components/signup/signup-platforms.js
@@ -8,15 +8,15 @@ export default function SignupPlatforms() {
         <section className="w-full sm:w-1/2 flex flex-col gap-3">
             <button 
                 type='submit'
-                className="flex text-primary font-medium items-center gap-4 bg-transparent rounded px-4 py-4 border-2 border-black text-left"
+                className="sign-up-platform-button flex text-primary font-medium items-center gap-4 bg-transparent rounded px-4 py-4 border-[1.4px] text-left"
             ><AiOutlineGoogle className="scale-150"/> Google</button>
             <button 
                 type='submit'
-                className="flex text-primary items-center gap-4 bg-transparent rounded px-4 py-4 border-2 border-black font-medium text-left"
+                className="sign-up-platform-button sign-up-facebook flex text-primary items-center gap-4 bg-transparent rounded px-4 py-4 border-[1.4px] font-medium text-left"
             ><FaFacebookF className="scale-150"/> Facebook</button>
             <button 
                 type='submit'
-                className="flex text-primary items-center gap-4 bg-transparent rounded px-4 py-4 border-2 border-black font-medium text-left"
+                className="sign-up-platform-button flex text-primary items-center gap-4 bg-transparent rounded px-4 py-4 border-[1.4px] font-medium text-left"
             ><AiFillApple className="scale-150"/> Apple</button>
         </section>
     )
