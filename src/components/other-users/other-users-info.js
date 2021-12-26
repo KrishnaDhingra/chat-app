69ea95d51bc9ffb6cfc793645db3a4c1ab154be2
Krishna Dhingra <dkrishna1608@gmail.com>
2021-12-26 22:30:21 +0530

    Now if the user doesn't have a display picture then a random avatar will show up

--- src/components/other-users/other-users-info.js
+++ src/components/other-users/other-users-info.js
@@ -1,15 +1,21 @@
 import React from "react"
 import { FiSettings } from "react-icons/fi"
 import { authentication } from "../../firebase"
+import { BigHead } from "@bigheads/core";
+import { getRandomOptions  } from "../bigheads";
 
 export default function OtherUsersInfo() {
 
     const user = authentication.currentUser
+    user.photoURL  = null
 
     return (
         <section className="relative w-full z-0 h-full flex items-center justify-center flex-col bg-transparent">
             <FiSettings className="text-primary absolute top-7 right-7"/>
-            <img src={user.photoURL} alt="Other user avatar" className="h-[90px] aspect-square rounded-full mb-4"/>
+            {user.photoURL ? 
+                <img src={user.photoURL} alt="Other user avatar" className="h-[90px] aspect-square rounded-full mb-4"/> : 
+                <BigHead className="h-[90px] aspect-square" {...getRandomOptions()} />
+            }
             <h2 className="text-primary font-semibold">{user.displayName}</h2>
             <p className="text-xs text-secondary">My account</p>
         </section>
