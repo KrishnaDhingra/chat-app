77731eb26095bc03ca92812caccb914b208b8ed0
Krishna Dhingra <dkrishna1608@gmail.com>
2022-01-17 04:31:20 +0530

    Adding friends icon in the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { MdOutlineLogout } from "react-icons/md";
-import { AiFillHome, AiOutlineQuestionCircle, AiOutlineCloudDownload } from "react-icons/ai";
-import { BiNotepad } from "react-icons/bi";
+import { AiFillHome, AiOutlineQuestionCircle } from "react-icons/ai";
+import { FiSettings } from "react-icons/fi";
+import { FaUserFriends } from "react-icons/fa";
 
 
 export default function Navbar({ onSelect }) {
@@ -11,9 +12,9 @@ export default function Navbar({ onSelect }) {
             className="left-[-70px] z-10 sm:left-[0px] h-full w-[70px] sm:w-full bg-[#268D61] absolute sm:relative flex flex-col items-center gap-6 py-5 sm:rounded-l-md">
 
             <AiFillHome className="hover:text-white hover:opacity-100 text-gray-300 text-2xl font-bold opacity-70"/>
-            <BiNotepad className="mt-4 hover:text-white hover:opacity-100 text-gray-300 text-xl font-bold opacity-70"/>
-            <AiOutlineCloudDownload className="hover:text-white hover:opacity-100 text-gray-300 text-xl font-bold opacity-70"/>
-            <AiOutlineQuestionCircle className="mt-auto hover:text-white hover:opacity-100 text-gray-300 text-xl font-bold opacity-70"/>
+            <FaUserFriends className="mt-4 hover:text-white hover:opacity-100 text-gray-300 text-xl font-bold opacity-70"/>
+            <AiOutlineQuestionCircle className="hover:text-white hover:opacity-100 text-gray-300 text-xl font-bold opacity-70"/>
+            <FiSettings className="mt-auto hover:text-white hover:opacity-100 text-gray-300 text-xl font-bold opacity-70"/>
             <MdOutlineLogout 
                 onClick={() => onSelect()}
                 className="hover:text-white hover:opacity-100 text-gray-300 text-xl font-bold opacity-70"/>
