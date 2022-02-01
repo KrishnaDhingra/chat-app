7a94a8022ab6f08107bf626f79608d0966607243
Krishna Dhingra <dkrishna1608@gmail.com>
2022-02-02 00:01:06 +0530

    Changing the identation a bit

--- src/components/friends/friends-search.js
+++ src/components/friends/friends-search.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function SearchBar({onChange}) {
     return (
         <input 
-        onChange={(e) => onChange(e.target.value)}
-        type="text" 
-        placeholder="Search for a friend..." 
-        className="bg-inputBg px-3 py-2 rounded-md text-primary outline-none border border-transparent focus:border-focus placeholder:text-sm w-full"></input>
+            onChange={(e) => onChange(e.target.value)}
+            type="text" 
+            placeholder="Search for a friend..." 
+            className="bg-inputBg px-3 py-2 rounded-md text-primary outline-none border border-transparent focus:border-focus placeholder:text-sm w-full"></input>
     )
 }
