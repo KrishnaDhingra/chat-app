ebffb69382e9c27ff8fbfa05ab22d74029bd9da5
Krishna Dhingra <dkrishna1608@gmail.com>
2022-02-05 09:57:30 +0530

    Making styles according to the pages, now just have to style the settings page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import "./index.css"
 import Home from './pages/home'
 import Settings from './pages/settings'
 import Navbar from './components/navbar'
+import SigninContainer from './components/signin/signin-container'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { authentication } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth'
 
 const App = () => {
 
+  const [ user, setUser ] = useState(null)
   const [ visibility, setVisibility ] = useState(false)
   const [ screen , setScreen ] = useState('friends')
+  const [ theme, setTheme ] = useState('theme-dark')
 
+  const toggleTheme = () => {
+    if(theme == 'theme-dark'){return setTheme('theme-light')}
+    setTheme('theme-dark')
+  }
   const toggleVisibility = () => {
     setVisibility(!visibility)
   }
@@ -17,22 +26,37 @@ const App = () => {
     setScreen(screenType)
   }
 
-  return(
-    <Router>
-
-      {/* <Navbar changeScreen={changeScreen} onSelect={toggleVisibility}/> */}
-
-      <Switch>
-        <Route className="" path="/" exact>
-          <Home visibility={visibility} screen={screen} changeScreen={changeScreen} toggleVisibility={toggleVisibility}/>
-        </Route>
-
-        <Route path="/settings" exact>
-          <Settings/>
-        </Route>
-      </Switch>
-
-    </Router>
+  onAuthStateChanged(authentication, (user) => {
+    if(user){
+        return setUser(user)
+    }
+    setUser(null)
+  })
+  if(user){
+    return(
+      <Router>
+        <div className={`${theme} main-container lg:max-h-[92vh] max-w-[1340px] w-screen rounded-md`}>
+  
+          <Navbar changeScreen={changeScreen} onSelect={toggleVisibility}/>
+          
+          <Switch>
+            
+            <Route className="" path="/" exact>
+              <Home visibility={visibility} screen={screen} changeScreen={changeScreen} toggleVisibility={toggleVisibility} toggleTheme={toggleTheme} theme={theme}/>
+            </Route>
+  
+            <Route path="/settings" exact>
+              <Settings/>
+            </Route>
+          </Switch>
+        </div>
+  
+  
+      </Router>
+    )
+  }
+  return (
+    <SigninContainer theme={theme}/>
   )
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,59 +1,37 @@
 import React, { useState } from 'react'
 import OtherUsersContainer from '../components/other-users/other-users-container.js'
-import Navbar from '../components/navbar.js';
 import MainChatsContainer from '../components/chats/main-chats-container';
 import FriendsContainer from '../components/friends/friends-container'
 import SigninContainer from '../components/signin/signin-container.js'
 import NotificationContainer from '../components/notifications/notification-container';
 import ConfirmationPopup from '../components/confirmation-popup'
-import { motion, AnimatePresence } from 'framer-motion'
-import { authentication } from '../firebase';
-import { onAuthStateChanged } from 'firebase/auth'
+import { AnimatePresence } from 'framer-motion'
 
-const Home = ({visibility, screen, changeScreen, toggleVisibility}) => {
+const Home = ({visibility, screen, changeScreen, toggleVisibility, toggleTheme, theme}) => {
 
-  const [ theme, setTheme ] = useState('theme-dark')
-  const [ user, setUser ] = useState(null)
   const [ chat, setChat ] = useState(null)
 
-  const toggleTheme = () => {
-    if(theme == 'theme-dark'){return setTheme('theme-light')}
-    setTheme('theme-dark')
-  }
   const selectedUser = (user) => {
     setChat(user)
   }
-  onAuthStateChanged(authentication, (user) => {
-    if(user){
-        return setUser(user)
-    }
-    setUser(null)
-  })
-  if(user){
-    return (
-      <div className={`theme-dark relative main-container lg:max-h-[92vh] max-w-[1340px] w-screen h-screen bg-secondary rounded-md shadow-xl bg-blue-300`}>
-
-        <Navbar changeScreen={changeScreen} onSelect={toggleVisibility}/>
-
-        <OtherUsersContainer selectedUser={selectedUser} changeScreen={changeScreen}/>
-
-        {screen == 'messages' && <MainChatsContainer onSelect={toggleTheme} otherUser={chat}/>}
-
-        {((!chat && screen != 'notifications') || (screen == 'friends')) && <FriendsContainer onSelect={toggleTheme}/>}
-
-        {screen == 'notifications' && <NotificationContainer onSelect={toggleTheme}/>}
-        
-        <AnimatePresence>
-          {visibility && 
-              <ConfirmationPopup onSelect={toggleVisibility}/>
-          }
-        </AnimatePresence>
-      </div>
-
-    )
-  }
   return (
-    <SigninContainer theme={theme}/>
+    <div className="relative home-container w-full h-full bg-secondary rounded-md shadow-xl">
+
+      <OtherUsersContainer selectedUser={selectedUser} changeScreen={changeScreen}/>
+
+      {screen == 'messages' && <MainChatsContainer onSelect={toggleTheme} otherUser={chat}/>}
+
+      {((!chat && screen != 'notifications') || (screen == 'friends')) && <FriendsContainer onSelect={toggleTheme}/>}
+
+      {screen == 'notifications' && <NotificationContainer onSelect={toggleTheme}/>}
+      
+      <AnimatePresence>
+        {visibility && 
+            <ConfirmationPopup onSelect={toggleVisibility}/>
+        }
+      </AnimatePresence>
+    </div>
+
   )
 }
 
