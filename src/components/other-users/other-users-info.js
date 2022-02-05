2047e7b3db558d701d86a1ced0d652c336bf7ffb
Krishna Dhingra <dkrishna1608@gmail.com>
2022-02-05 10:18:32 +0530

    Putting static flie in user avatar for now

--- src/components/other-users/other-users-info.js
+++ src/components/other-users/other-users-info.js
@@ -6,10 +6,9 @@ export default function OtherUsersInfo() {
 
     const user = authentication.currentUser
     return (
-        <section className="relative w-full z-0 h-full flex items-center justify-center flex-col bg-transparent">
-            <FiSettings className="text-primary absolute top-7 right-7"/>
+        <section className="w-full z-0 h-full flex items-center justify-center flex-col bg-transparent">
 
-            <img src={user.photoURL} alt="Other user avatar" className="h-[90px] aspect-square rounded-full mb-4"/>
+            <img src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Other user avatar" className="h-[100px] aspect-square rounded-full mb-4 object-cover"/>
                 
             <h2 className="text-primary font-semibold">{user.displayName}</h2>
             
