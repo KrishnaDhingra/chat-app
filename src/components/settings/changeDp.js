6830225197df6cda676efca06f30d65dc757a1ad
Krishna Dhingra <dkrishna1608@gmail.com>
2022-02-07 10:42:07 +0530

    Adding camera hover effect on settings dp

--- src/components/settings/changeDp.js
+++ src/components/settings/changeDp.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { BsCameraFill } from 'react-icons/bs'
 const ChangeDp = () => {
     return (
         <div className="flex gap-4">
-            <img src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Other user avatar" className="h-[80px] aspect-square rounded-full mb-4 object-cover"/>
+            <div className="relative changeDp-image-outer h-[80px]">
+                <img src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Other user avatar" className="h-full aspect-square rounded-full mb-4 object-cover z-1"/>
+                <BsCameraFill className="hover-camera absolute text-white text-xl"/>
+            </div>
 
             <div className="mt-4 text-primary font-medium text-[16px]">Krishna Dhingra</div>
         </div>
