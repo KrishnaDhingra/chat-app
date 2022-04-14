0842a1931da55cc34b811d6aa507bcf43c322b4f
Krishna Dhingra <dkrishna1608@gmail.com>
2022-04-14 22:45:43 +0530

    Added the functionality to remove friend and getUsername from id

--- src/components/friends/friend-bar.js
+++ src/components/friends/friend-bar.js
@@ -1,24 +1,65 @@
-import React from 'react'
-import { RiMessageFill } from "react-icons/ri";
-import { IoMdRemove } from "react-icons/io";
+import React, { useState } from 'react'
+import { RiMessageFill } from 'react-icons/ri'
+import { IoMdRemove } from 'react-icons/io'
+import ConfirmationPopup from '../confirmation-popup'
+import { AnimatePresence } from 'framer-motion'
+import { db, authentication } from '../../firebase'
+import { doc, deleteDoc } from 'firebase/firestore'
 
-function FriendBar({friend}) {
-    return (
-        <div className="relative friends-bar h-[68px] w-full bg-gray-chat-preview items-center flex gap-3">
-            <img src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="other users dp" className="h-10 aspect-square rounded-full" />
-            <div className="h-full justify-center flex flex-col grow">
-                <span className="font-semibold text-sm text-primary">Krishna Dhingra</span>
-                <span className="text-xs text-secondary text-normal">offline</span>
-            </div>
-            <div className="flex flex-row gap-4 text-sm text-secondary mr-7">
-                <div className="p-2 icon-outer">
-                    <RiMessageFill className="friends-bar-icons"/>
-                </div>
-                <div className="p-2 rounded-full bg-red-700">
-                    <IoMdRemove className="friends-bar-icons text-black"/>
-                </div>
-            </div>
-        </div>
+function FriendBar({ friend }) {
+  const [visibility, setVisibility] = useState(false)
+  let toggleVisibility = () => {
+    setVisibility(false)
+  }
+
+  let removeFriend = async () => {
+    const docRef = doc(
+      db,
+      'friends',
+      authentication.currentUser.uid,
+      'friends',
+      friend.friendId,
     )
+    await deleteDoc(docRef)
+    console.log('Deleted Friend')
+  }
+  return (
+    <div className="friends-bar h-[68px] w-full bg-gray-chat-preview items-center flex gap-3">
+      <img
+        src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+        alt="other users dp"
+        className="h-10 aspect-square rounded-full"
+      />
+      <div className="h-full justify-center flex flex-col grow">
+        <span className="font-semibold text-sm text-primary">
+          Krishna Dhingra
+        </span>
+        <span className="text-xs text-secondary text-normal">offline</span>
+      </div>
+      <div className="flex flex-row gap-4 text-sm text-secondary mr-7">
+        <div className="icon-outer">
+          <RiMessageFill />
+        </div>
+        <div
+          onClick={() => setVisibility(true)}
+          className="bg-red-700 rounded-[50%] p-[0.7rem] hover:bg-red-800"
+        >
+          <IoMdRemove className="text-black" />
+        </div>
+      </div>
+      <AnimatePresence>
+        {visibility && (
+          <ConfirmationPopup
+            onSelect={toggleVisibility}
+            operation={removeFriend}
+            heading={'Remove from friends'}
+            text={
+              'Are you sure you want to remove this user from your friends ?'
+            }
+          />
+        )}
+      </AnimatePresence>
+    </div>
+  )
 }
 export default FriendBar
--- src/components/friends/friends-container.js
+++ src/components/friends/friends-container.js
@@ -4,115 +4,124 @@ import SearchBar from './friends-search'
 import AddFriendSearchBar from './add-friend-searchbar'
 import FriendBar from './friend-bar'
 import AddFriendBar from './add-friend-bar'
-import { db, authentication } from '../../firebase';
-import {
-  collection,
-  onSnapshot,
-  query,
-  where
-} from 'firebase/firestore'
+import { db, authentication } from '../../firebase'
+import { collection, onSnapshot, query, where } from 'firebase/firestore'
+import getUsername from '../notifications/getUsername'
 
+function FriendsContainer({ onSelect }) {
+  // determines that the total friends screen or the add friend screen should be shown. If this is true then the total friends screen will be shown and if false the add friend screen will be shown
+  const [friendsScreen, setFriendsScreen] = useState(true)
+  // holds the list of all the friends that the user has
+  const [totalFriends, setTotalFriends] = useState([])
+  // holds the list of the filtered friends based on the search
+  const [filteredFriends, setFilteredFriends] = useState([])
+  // holds the list of all the users that are on the platform
+  const [totalAddFriends, setTotalAddFriends] = useState([])
+  // holds the list of the filtered add friends based on the search
+  const [filteredAddFriends, setFilteredAddFriends] = useState([])
 
-function FriendsContainer({onSelect}){
+  const friendsRef = collection(
+    db,
+    'friends',
+    authentication.currentUser.uid,
+    'friends',
+  )
 
-    // determines that the total friends screen or the add friend screen should be shown. If this is true then the total friends screen will be shown and if false the add friend screen will be shown
-    const [ friendsScreen, setFriendsScreen ] = useState(true)
-    // holds the list of all the friends that the user has
-    const [ totalFriends, setTotalFriends ] = useState([])
-    // holds the list of the filtered friends based on the search
-    const [ filteredFriends, setFilteredFriends ] = useState([])
-    // holds the list of all the users that are on the platform
-    const [ totalAddFriends, setTotalAddFriends ] = useState([])
-    // holds the list of the filtered add friends based on the search
-    const [ filteredAddFriends, setFilteredAddFriends ] = useState([])
+  const addFriendsRef = collection(db, 'users')
+  const q2 = query(
+    addFriendsRef,
+    where('uid', 'not-in', [authentication.currentUser.uid]),
+  )
 
-    
-    const friendsRef = collection(db, 'friends', authentication.currentUser.uid, 'friends')
-    
-    const addFriendsRef = collection(db, 'users')
-    const q2 = query(addFriendsRef, where('uid', 'not-in', [authentication.currentUser.uid]))
-
-
-    // this toggles friedsScreen state variable between true and false
-    const toggleFriendsScreen = (value) => {
-        setFriendsScreen(value)
+  // this toggles friedsScreen state variable between true and false
+  const toggleFriendsScreen = (value) => {
+    setFriendsScreen(value)
+  }
+  // this loads all the friends the the user has and assigs them to the 'friends' state variable
+  useEffect(() => {
+    const loadFriends = onSnapshot(friendsRef, (snapshot) => {
+      let friends = []
+      snapshot.docs.forEach((doc) => {
+        friends.push(doc.data())
+      })
+      setTotalFriends(friends)
+      setFilteredFriends(friends)
+    })
+    const loadAddFriends = onSnapshot(q2, (snapshot) => {
+      let addFriends = []
+      snapshot.docs.forEach((doc) => {
+        addFriends.push({ ...doc.data(), id: doc.uid })
+      })
+      setTotalAddFriends(addFriends)
+    })
+    return () => {
+      loadFriends()
+      loadAddFriends()
     }
-    // this loads all the friends the the user has and assigs them to the 'friends' state variable
-    useEffect(() => {
-        const loadFriends = onSnapshot(friendsRef, (snapshot) => {
-    
-            let friends = []
-            snapshot.docs.forEach(doc => {
-                friends.push(doc.data())
-            })
-            setTotalFriends(friends)
-        })
-        const loadAddFriends = onSnapshot(q2, (snapshot) => {
-    
-            let addFriends = []
-            snapshot.docs.forEach(doc => {
-                addFriends.push({...doc.data(), id:doc.uid})
-            })
-            setTotalAddFriends(addFriends)
-        })
-        return () => {
-            loadFriends()
-            loadAddFriends()
-        }
-    }, [])
+  }, [])
 
-    // this filter the friends state variables and then assigns the filtered value to the 'filteredFriends' state variable which inturn is used to render the 'friendBar' component
-    const filterFriends = (userName) => {
-
-        let filteredFriends = totalFriends.filter(friend => friend.displayName.toLowerCase().includes(userName.toLowerCase()))
-
-        filteredFriends ? setFilteredFriends(filteredFriends) : setFilteredFriends([])
-    }
-    const filterAddFriends = (userName) => {
-
-        let filteredAddFriends = totalAddFriends.filter(friend => friend.displayName.toLowerCase().includes(userName.toLowerCase()))
-
-        filteredAddFriends ? setFilteredAddFriends(filteredAddFriends) : setFilteredAddFriends([])
-    }
-    return (
-        
-        <main className="z-0 bg-chatContainer flex flex-col gap-6 h-[100vh] lg:max-h-[92vh] lg:w-full py-7 pl-4 sm:pl-10 rounded-r-md m-0">
-
-            <section className="flex flex-col gap-6 w-full pr-4 sm:pr-10">
-                <section className="flex w-full">
-
-                    <p onClick={() => toggleFriendsScreen(true)} className="text-primary text-3xl font-semibold">Friends</p>
-                    <button 
-                        onClick={() => toggleFriendsScreen(false)} 
-                        className="add-friend-button bg-navbarBg rounded-md text-primary font-semibold ml-auto mr-6 text-sm py-2 px-4"
-                        disabled={!friendsScreen}
-                    >Add Friend
-                    </button>
-                    <ModeSwitcher onSelect={onSelect}/>
-
-                </section>
-                {friendsScreen ? <SearchBar onChange={filterFriends}/> : <AddFriendSearchBar onChange={filterAddFriends}/> }
-            </section>
-
-            <div className="h-full w-full main-chats-container overflow-y-auto sm:pr-10 pr-4">
-
-                {
-                    friendsScreen ? filteredFriends.length > 0 && filteredFriends.map(friend => {
-                        return <FriendBar key={friend.uid} friend={friend}/>
-                    }) : 
-                    <div>
-                        {
-                            filteredAddFriends.length > 0 && filteredAddFriends.map(user => {
-                                return <AddFriendBar key={user.uid} user={user}/>
-                            })
-                        }
-                    </div>
-                }
-            </div>
-            
-            
-        </main>
-
+  // this filter the friends state variables and then assigns the filtered value to the 'filteredFriends' state variable which inturn is used to render the 'friendBar' component
+  const filterFriends = async (userName) => {
+    // let filteredFriends = totalFriends.filter((friend) => {
+    //   let displayName2 = await getUsername(friend.friendId)
+    //   displayName2.includes(userName.toLowerCase())
+    // })
+    // filteredFriends
+    //   ? setFilteredFriends(filteredFriends)
+    //   : setFilteredFriends([])
+  }
+  const filterAddFriends = (userName) => {
+    console.log(userName)
+    let filteredAddFriends = totalAddFriends.filter((friend) =>
+      friend.displayName.toLowerCase().includes(userName.toLowerCase()),
     )
+
+    filteredAddFriends
+      ? setFilteredAddFriends(filteredAddFriends)
+      : setFilteredAddFriends([])
+  }
+  return (
+    <main className="z-0 bg-chatContainer flex flex-col gap-6 h-[100vh] lg:max-h-[92vh] lg:w-full py-7 pl-4 sm:pl-10 rounded-r-md m-0">
+      <section className="flex flex-col gap-6 w-full pr-4 sm:pr-10">
+        <section className="flex w-full">
+          <p
+            onClick={() => toggleFriendsScreen(true)}
+            className="text-primary text-3xl font-semibold"
+          >
+            Friends
+          </p>
+          <button
+            onClick={() => toggleFriendsScreen(false)}
+            className="add-friend-button bg-navbarBg rounded-md text-primary font-semibold ml-auto mr-6 text-sm py-2 px-4"
+            disabled={!friendsScreen}
+          >
+            Add Friend
+          </button>
+          <ModeSwitcher onSelect={onSelect} />
+        </section>
+        {friendsScreen ? (
+          <SearchBar onChange={filterFriends} />
+        ) : (
+          <AddFriendSearchBar onChange={filterAddFriends} />
+        )}
+      </section>
+
+      <div className="h-full w-full main-chats-container overflow-y-auto sm:pr-10 pr-4">
+        {friendsScreen ? (
+          filteredFriends.length > 0 &&
+          filteredFriends.map((friend) => {
+            return <FriendBar key={friend.uid} friend={friend} />
+          })
+        ) : (
+          <div>
+            {filteredAddFriends.length > 0 &&
+              filteredAddFriends.map((user) => {
+                return <AddFriendBar key={user.uid} user={user} />
+              })}
+          </div>
+        )}
+      </div>
+    </main>
+  )
 }
 export default FriendsContainer
